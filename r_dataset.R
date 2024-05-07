7b43fa4f6e86b5185997d8258ccae6d971171399
justinekum <156072732+justinekum@users.noreply.github.com>
2024-05-07 12:38:40 +0100

    dataset upload

--- r_dataset.R
+++ r_dataset.R
@@ -1,5 +1,5 @@
 library(haven)
 library(foreign)
-data <- read.spss("/Users/justinekum/Desktop/dnp/term_3/dap_project/team_1_project/q1_dataset.sav", to.data.frame = T)
+dataset <- read.spss("/Users/justinekum/Desktop/dnp/term_3/dap_project/team_1_project/q1_dataset.sav", to.data.frame = T)
 
-data
+dataset
