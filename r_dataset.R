aa95028435073d8907aacb30b2a8d05c8bf01003
justinekum <156072732+justinekum@users.noreply.github.com>
2024-05-21 09:20:42 +0100

    re-organised dataset directory

--- r_dataset.R
+++ r_dataset.R
@@ -1,5 +1,5 @@
 library(haven)
 library(foreign)
 
-dataset_1 <- read.spss("/Users/justinekum/Desktop/dnp/term_3/dap_project/team_1_project/q1_dataset.sav", to.data.frame = T)
-dataset1
+dataset <- read.spss("q1_dataset.sav", to.data.frame = T)
+dataset
